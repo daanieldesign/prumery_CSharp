ff06e046e634e8f2fb06768b72d5a079bf0a0003
daanieldesign <mahdakdaniel@gmail.com>
2025-02-10 09:32:04 +0100

    update and fix

--- procvicovani/Program.cs
+++ procvicovani/Program.cs
@@ -1,74 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tridy_procvicovani
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Zadejte jméno studenta: ");
+            // Vytvoření nového studenta
+            Console.WriteLine("Zadejte jméno studenta");
             Student s1 = new Student();
             s1.Jmeno = Console.ReadLine();
+
             Student s2 = new Student();
             Student s3 = new Student();
-            Console.WriteLine("Zadejte 5 známek: ");
+
+            Console.WriteLine("Zadejte 5 známek");
             try
             {
                 for (int i = 0; i < 5; i++)
                 {
-                    s1.znamky.Add(Convert.ToInt32(Console.ReadLine()));
+                    s1.AddGrade(Convert.ToInt32(Console.ReadLine()));
                 }
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Console.WriteLine("Něco se pokazilo.");
+                Console.WriteLine("Něco se pokazilo při zadávání známek");
             }
 
             s1.KontrolaPrumeru();
-            Console.WriteLine("počet studentů: " + Student.seznamStudentu);
-            Console.ReadKey();
-            // - přidání známky přepište na vlastní metodu (v rámci třídy)
 
+            Console.WriteLine("Počet studentů: " + Student.seznamStudentu.Count);
+
+            Student.DisplayStudentNames();
+
+            s1.ModifyGrade(2, 5);
+
+            Student.DisplayStudentNames();
+
+            Console.ReadKey();
         }
     }
+
     public class Student
     {
         public string Jmeno;
         public double PrumernaZnamka;
         public List<int> znamky;
-        public static int seznamStudentu;
+        public static List<Student> seznamStudentu = new List<Student>();
+
         public Student()
         {
-            znamky = new List<int>();
-            seznamStudentu++;
+            znamky = new List<int>(); 
+            seznamStudentu.Add(this);
         }
-
-        public void Znamky()
-        {
-            Console.WriteLine("Zadejte 5 známek: ");
-            try
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    znamky.Add(Convert.ToInt32(Console.ReadLine()));
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Něco se pokazilo.");
-            }
-        }
-
 
         public void ZobrazInfo()
         {
             Console.WriteLine("Jméno studenta: " + Jmeno + "\nJeho průměr: " + PrumernaZnamka);
         }
+
         public void KontrolaPrumeru()
         {
             try
@@ -83,9 +75,36 @@ namespace Tridy_procvicovani
                     Console.WriteLine("Student nemá nárok na stipendium.");
                 }
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Console.WriteLine("Něco se pokazilo.");
+                Console.WriteLine("Něco se pokazilo při výpočtu průměru");
+            }
+        }
+
+        public void AddGrade(int grade)
+        {
+            znamky.Add(grade); 
+        }
+
+        public static void DisplayStudentNames()
+        {
+            Console.WriteLine("Jména všech studentů:");
+            foreach (var student in seznamStudentu)
+            {
+                Console.WriteLine(student.Jmeno);
+            }
+        }
+
+        public void ModifyGrade(int index, int newGrade)
+        {
+            if (index >= 0 && index < znamky.Count)
+            {
+                znamky[index] = newGrade;
+                Console.WriteLine("Známka na pozici " + index + " byla upravena na " + newGrade);
+            }
+            else
+            {
+                Console.WriteLine("Neplatný index.");
             }
         }
     }
